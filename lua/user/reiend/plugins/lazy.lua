e5c53da9149c5312010a6f0dba34f23c2e613611
reiend <loncerasjakerei@gmail.com>
2023-10-11 04:47:37 +0800

    lualine setup

--- lua/user/reiend/plugins/lazy.lua
+++ lua/user/reiend/plugins/lazy.lua
@@ -25,5 +25,6 @@ require('lazy').setup {
   get_config { plugin = require 'user.reiend.plugins.nvim-treesitter' },
   get_config { plugin = require 'user.reiend.plugins.efmls-configs-nvim' },
   get_config { plugin = require 'user.reiend.plugins.telescope' },
+  get_config { plugin = require 'user.reiend.plugins.lualine' },
 }
 
